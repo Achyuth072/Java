b074a09182573f149287297cf20a1e5479f304eb
Achyuth Suchit <achyuthsuchit@gmail.com>
2022-02-15 10:29:34 +0530

    Updated Largeorsmall.java

--- Largeorsmall.java
+++ Largeorsmall.java
@@ -1,13 +1,21 @@
 package main;
+import java.util.*;
 
 public class Largeorsmall {
 
 	public static void main(String[] args) {
-		int l,s,i;
-		int a[] = new int[]{3, 5, 8, 1, 6};
-		int n = a.length;
+		int l,s,n,i;		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter Number of elements : ");
+		n=sc.nextInt();		
+		int a[] = new int[n];
+		System.out.println("Enter elements : ");
+		for(i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}		
 		l=s=a[0];
-		for(i=1;i<n;++i)
+		for(i=0;i<n;++i)
 		{
 			if(a[i]>l)
 				l=a[i];
