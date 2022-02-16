901c86517db413afa473bd336d601c34379ce413
Achyuth Suchit <achyuthsuchit@gmail.com>
2022-02-16 16:52:31 +0530

    updated MethodOverloading.java

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -4,33 +4,34 @@ import java.util.*;
 public class MethodOverloading {
 
 	public static void main(String[] args) {
+		MethodOverloading m=new MethodOverloading();		
 		Scanner in= new Scanner(System.in);
 		System.out.println("Enter Side of square : ");
 		float a=in.nextFloat();
-		System.out.println("Area of square =   "+area(a));
+		System.out.println("Area of square =   "+m.area(a));
 		System.out.println("Enter length and breath of rectagle : ");
 		float l=in.nextFloat();
 		float b=in.nextFloat();
-		System.out.println("Area of rectangle =   "+area(l,b));
+		System.out.println("Area of rectangle =   "+m.area(l,b));
 		System.out.println("Enter radius of circle : ");
 		double r=in.nextDouble();
-		System.out.println("Area of circle =   "+area(r));
+		System.out.println("Area of circle =   "+m.area(r));
 		
 		// TODO Auto-generated method stub
 
 	}
 	
-	static float area(float a)
+	float area(float a)
 	{ return a*a;
 	
 	}
 	
-	static float area(float l, float b)
+	float area(float l, float b)
 	{
 		return l*b;
 	}
 	
-	static double area(double r)
+	double area(double r)
 	{
 		return 3.14*r*r;
 	}
